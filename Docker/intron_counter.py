8809db930c3fe54262742d8441ffe56473d984f9
Brian Haas <brianjohnhaas@gmail.com>
2020-06-01 20:53:16 +0000

    add

--- Docker/intron_counter.py
+++ Docker/intron_counter.py
@@ -4,42 +4,42 @@ import sys, os, re
 import pysam
 from collections import defaultdict
 
+
 def main():
 
-    usage = "usage: {} input.bam region\n\tregion format is chr?:lend-rend\n\n".format(sys.argv[0])
+    usage = "usage: {} input.bam sample_name\n\n".format(sys.argv[0])
     if len(sys.argv) < 3:
         sys.stderr.write(usage)
         sys.exit(1)
 
     input_bam_filename = sys.argv[1]
-    region = sys.argv[2]
+    sample_name = sys.argv[2]
 
     intron_counter = defaultdict(int)
-    
+
     samfile = pysam.AlignmentFile(input_bam_filename, "rb")
     for read in samfile.fetch():
         blocks = read.get_blocks()
+
         num_blocks = len(blocks)
-        if num_blocks == 1:
+        if num_blocks < 2:
             continue
 
         chr_name = samfile.get_reference_name(read.reference_id)
-        
-        for i in range(num_blocks):
+
+        for i in range(num_blocks - 1):
             intron_lend = blocks[i][1] + 1 + 1
-            intron_rend = blocks[i+1][0] + 1 - 1
+            intron_rend = blocks[i + 1][0] + 1 - 1
 
             intron_name = "{}:{}-{}".format(chr_name, intron_lend, intron_rend)
 
             intron_counter[intron_name] += 1
 
-
     for intron, count in intron_counter.items():
-        print("\t".join(intron, count))
-        
+        print("\t".join([sample_name, intron, str(count)]))
 
     sys.exit(0)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
